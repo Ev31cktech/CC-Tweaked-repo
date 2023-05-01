0ac0ada6f5a00a7e4fb0dd31141b559e1b623144
Rick van Haandel <I461577@student.fontys.nl>
2023-05-01 15:26:42 +0200

    add Stckpile.lua

---
+++ Stockpile.lua
@@ -0,0 +1,14 @@
+local drawer = peripheral.wrap("back");
+drawer.getAmount = drawer_getAmount;
+minimum = 64
+
+function drawer'_getAmount() 
+	return drawer.list()[2].count
+end
+
+while true do
+	local value = drawer.getAmount() > 64
+	for side in redstone.getSides() do
+		redstone.setOutput(side , value);
+	end
+end
